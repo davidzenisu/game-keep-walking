c8f66cb61d87e20f5acc3d09689ba0ff21afba30
David Zeni-Su <32648667+davidzenisu@users.noreply.github.com>
2026-06-12 19:53:02 +0000

    Fix analysis issues

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
 }
 
 class GameScreen extends StatefulWidget {
-  const GameScreen({Key? key}) : super(key: key);
+  const GameScreen({super.key});
 
   @override
   State<GameScreen> createState() => _GameScreenState();
